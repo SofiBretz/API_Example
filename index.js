2bc58fe1e29f371d1ee84f72dec86acf541a40b1
SofiBretz <sofiabarrantes.gamboa@gmail.com>
2020-11-15 11:11:17 -0600

    Cors error fixed

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 document.querySelector("#myForm").addEventListener("submit", function(e){
+    var request = new XMLHttpRequest();
+    let url = `https://ltv-data-api.herokuapp.com/api/v1/records.json?email=${inputVal}`;
+    request.open("GET", url, false);
+    request.send(null);
     e.preventDefault(); //stop form from submitting
     const inputVal = document.getElementById("inlineFormInput").value;
     fetch(`https://ltv-data-api.herokuapp.com/api/v1/records.json?email=${inputVal}`)
-//.then(response => response.json())
-    .then(data => console.log('--->',data)); 
+        .then(inputVal => {
+            if (request.status === 200) {
+                console.log('SUCCESS')
+            } else {
+                console.log("Not Successful")
+            }
+            
+        })
+    .then(data => console.log('--->',data))
+    .catch(error => console.log("ERROR"))
 }); 
+
+
+
